539b370e4d845c128d3c365e2eead58cb26c5730
Alexey Rodionov <leksurion@gmail.com>
2022-06-18 17:52:15 +0300

    Add the theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,42 @@
+export const COLOR = Object.freeze({
+  PRIMARY: '#1FCC79',
+  TEXT_PRIMARY: '#2E3E5C',
+  TEXT_SECONDARY: '#9FA5C0',
+  BACKGROUND: '#F4F5F7',
+  BORDER: '#D0DBEA',
+  WHITE: '#FFFFFF',
+  RED: '#FF6464',
+});
+
+export const FONT_FAMILY = Object.freeze({
+  REGULAR: 'Inter-Medium',
+  BOLD: 'Inter-Bold',
+});
+
+export const FONT_SIZE = Object.freeze({
+  SMALL: 12,
+  REGULAR: 15,
+  LARGE: 17,
+  EXTRA_LARGE: 22,
+});
+
+export const LINE_HEIGHT = Object.freeze({
+  SMALL: 15,
+  REGULAR: 25,
+  LARGE: 27,
+  EXTRA_LARGE: 32,
+});
+
+export const PADDING = Object.freeze({
+  EXTRA_SMALL: 4,
+  SMALL: 8,
+  REGULAR: 12,
+  LARGE: 16,
+  EXTRA_LARGE: 20,
+});
+
+export const ICON_SIZE = Object.freeze({
+  REGULAR: 36,
+  LARGE: 56,
+  EXTRA_LARGE: 64,
+});
