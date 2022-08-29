3ae0e65c033b56c65766ac1652e0177b369bcf87
Alexey Rodionov <alexeyxrodionov@gmail.com>
2022-08-29 16:23:53 +0300

    Change fetching the meal plan

--- src/screens/MealPlan/WeekPlan/index.tsx
+++ src/screens/MealPlan/WeekPlan/index.tsx
@@ -1,38 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ActivityIndicator, ScrollView, View} from 'react-native';
 import Modal from 'react-native-modal';
 
-import {MOCK_MEAL_PLAN, MOCK_MEAL_TYPES} from 'src/services/api/mealPlan';
+import {fetchMealPlanByDay, MOCK_MEAL_TYPES} from 'src/services/api/mealPlan';
+import {MealPlan} from 'src/services/api/types';
 import {COLOR} from 'src/constants/theme';
 import {WeekCalendar} from 'src/components';
 import MealTypeCard from './components/MealTypeCard';
 import s from './styles';
 
-// type NavigationType = StackNavigationProp<RecipeStackParamList>;
-
 const WeekPlan = () => {
   const [selectedDay, setSelectedDay] = useState(new Date());
+  const [selectedMealPlan, setSelectedMealPlan] = useState<MealPlan>();
+  const [loading, setLoading] = useState(false);
   // TODO: Get from Redux, fetch from API
   const mealTypes = MOCK_MEAL_TYPES;
-  const selectedMealPlan = MOCK_MEAL_PLAN;
 
   const handleSelectDay = (date: Date) => {
     setSelectedDay(date);
   };
-  // const dispatch = useDispatch();
-  // const navigation = useNavigation<NavigationType>();
 
-  // const recipes = useSelector((state: RootState) => state.recipe.list);
-  // const status = useSelector((state: RootState) => state.recipe.status);
-  // const loading = useMemo(() => status === RequestStatus.Loading, [status]);
+  useEffect(() => {
+    if (selectedDay) {
+      setLoading(true);
 
-  // useEffect(() => {
-  //   dispatch(recipeActions.fetchRecipes());
-  // }, [dispatch]);
+      fetchMealPlanByDay(selectedDay)
+        .then(setSelectedMealPlan)
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [selectedDay]);
 
   return (
     <View style={s.container}>
-      <Modal isVisible={false}>
+      <Modal isVisible={loading}>
         <ActivityIndicator size={'large'} color={COLOR.WHITE} />
       </Modal>
 
@@ -44,6 +46,10 @@ const WeekPlan = () => {
         {mealTypes &&
           mealTypes.length > 0 &&
           mealTypes.map(mealType => {
+            if (!selectedMealPlan) {
+              return null;
+            }
+
             const recipes =
               selectedMealPlan.recipes?.filter(
                 recipe => recipe.mealType.id === mealType.id,
--- src/services/api/mealPlan.ts
+++ src/services/api/mealPlan.ts
@@ -1,5 +1,7 @@
 // TODO: Fetch this and all reference data in one request at the run of app and put them all to redux (After MVP)
 import {MealPlan, MealType} from './types';
+import {sleep} from 'src/services/api/utils';
+import {sameDays} from 'src/utils/dateUtils';
 
 export const MOCK_MEAL_TYPES: MealType[] = [
   {
@@ -20,119 +22,109 @@ export const MOCK_MEAL_TYPES: MealType[] = [
   },
 ];
 
-export const MOCK_MEAL_PLAN: MealPlan = {
-  id: 1,
-  recipes: [
-    {
-      id: 1,
-      recipe: {
-        id: 'rp14',
-        name: 'ПП-хачапури',
-        cover:
-          'https://sun2-9.userapi.com/impg/P1fP70PDYaRcr3HJ5zGn3CCDA_b9ZuNWAfArEg/5s_WenZrQGw.jpg?size=1280x1280&quality=95&sign=1658e82d2d49344bc3f301e205567a0e&type=album',
-      },
-      servingCount: 2,
-      mealType: MOCK_MEAL_TYPES[0],
-    },
-    {
-      id: 2,
-      recipe: {
-        id: 'rp5',
-        name: 'Омлет "Рулет" с сыром и овощной салат',
-        cover: null,
-      },
-      servingCount: 1,
-      mealType: MOCK_MEAL_TYPES[0],
-    },
-    {
-      id: 3,
-      recipe: {
-        id: 'rp12',
-        name: 'Оладьи с шоколадом',
-        cover: null,
-      },
-      servingCount: 1,
-      mealType: MOCK_MEAL_TYPES[1],
-    },
-    {
-      id: 4,
-      recipe: {
-        id: 'rp5',
-        name: 'Омлет "Рулет" с сыром и овощной салат',
-        cover: null,
-      },
-      servingCount: 1,
-      mealType: MOCK_MEAL_TYPES[2],
-    },
-    {
-      id: 5,
-      recipe: {
-        id: 'rp12',
-        name: 'Оладьи с шоколадом',
-        cover: null,
-      },
-      servingCount: 1,
-      mealType: MOCK_MEAL_TYPES[2],
-    },
-  ],
-  products: [
-    {
-      id: 1,
-      product: {
-        id: 'p5',
-        name: 'Огурцы',
-        image: null,
-      },
-      quantity: {
-        value: 2,
-        unit: {
-          id: 'pu2',
-          name: 'штук',
-          shortName: 'шт',
-          nameEn: '',
-          shortNameEn: '',
+export const MOCK_MEAL_PLAN: MealPlan[] = [
+  {
+    id: 1,
+    recipes: [
+      {
+        id: 1,
+        recipe: {
+          id: 'rp14',
+          name: 'ПП-хачапури',
+          cover:
+            'https://sun2-9.userapi.com/impg/P1fP70PDYaRcr3HJ5zGn3CCDA_b9ZuNWAfArEg/5s_WenZrQGw.jpg?size=1280x1280&quality=95&sign=1658e82d2d49344bc3f301e205567a0e&type=album',
         },
+        servingCount: 2,
+        mealType: MOCK_MEAL_TYPES[0],
       },
-      mealType: MOCK_MEAL_TYPES[0],
-    },
-    {
-      id: 2,
-      product: {
-        id: 'p2',
-        name: 'Лук репчатый',
-        image: null,
-      },
-      quantity: {
-        value: 2,
-        unit: {
-          id: 'pu1',
-          name: 'грамм',
-          shortName: 'г',
-          nameEn: 'gram',
-          shortNameEn: 'g',
+      {
+        id: 2,
+        recipe: {
+          id: 'rp19',
+          name: 'Куриные котлеты с сыром',
+          cover: null,
         },
+        servingCount: 1,
+        mealType: MOCK_MEAL_TYPES[1],
       },
-      mealType: MOCK_MEAL_TYPES[1],
-    },
-    {
-      id: 3,
-      product: {
-        id: 'p17',
-        name: 'Творог Мягкий 5%',
-        image: null,
-      },
-      quantity: {
-        value: 2,
-        unit: {
-          id: 'pu1',
-          name: 'грамм',
-          shortName: 'г',
-          nameEn: 'gram',
-          shortNameEn: 'g',
+      {
+        id: 3,
+        recipe: {
+          id: 'rp10',
+          name: 'Рис с овощами',
+          cover: null,
         },
+        servingCount: 1,
+        mealType: MOCK_MEAL_TYPES[1],
       },
-      mealType: MOCK_MEAL_TYPES[1],
-    },
-  ],
-  day: '2022-08-28 00:00:00',
+    ],
+    products: [
+      {
+        id: 1,
+        product: {
+          id: 'p5',
+          name: 'Огурцы',
+          image: null,
+        },
+        quantity: {
+          value: 2,
+          unit: {
+            id: 'pu2',
+            name: 'штук',
+            shortName: 'шт',
+            nameEn: '',
+            shortNameEn: '',
+          },
+        },
+        mealType: MOCK_MEAL_TYPES[2],
+      },
+      {
+        id: 2,
+        product: {
+          id: 'p4',
+          name: 'Помидоры',
+          image: null,
+        },
+        quantity: {
+          value: 1,
+          unit: {
+            id: 'pu2',
+            name: 'штук',
+            shortName: 'шт',
+            nameEn: '',
+            shortNameEn: '',
+          },
+        },
+        mealType: MOCK_MEAL_TYPES[2],
+      },
+      {
+        id: 3,
+        product: {
+          id: 'p17',
+          name: 'Творог Мягкий 5%',
+          image: null,
+        },
+        quantity: {
+          value: 100,
+          unit: {
+            id: 'pu1',
+            name: 'грамм',
+            shortName: 'г',
+            nameEn: 'gram',
+            shortNameEn: 'g',
+          },
+        },
+        mealType: MOCK_MEAL_TYPES[0],
+      },
+    ],
+    day: '2022-09-01T00:00:00.000Z',
+  },
+];
+
+export const fetchMealPlanByDay = async (
+  day: Date,
+): Promise<MealPlan | undefined> => {
+  await sleep(0.5);
+
+  return MOCK_MEAL_PLAN.find(item => sameDays(new Date(item.day), day));
 };
